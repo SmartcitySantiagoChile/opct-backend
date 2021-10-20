2446fa95041f011083cc3a50f32022ec9b21f8fe
Bastianleaf <bastian.martinez@ing.uchile.cl>
2021-10-20 19:14:36 -0300

    Add all models

--- rest_api/models.py
+++ rest_api/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
-
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -39,18 +39,6 @@ class UserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
 
-class User(AbstractUser):
-    """User model."""
-
-    username = None
-    email = models.EmailField(_('email address'), unique=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    objects = UserManager()
-
-
 class ContractType(models.Model):
     name = models.TextField("Nombre", max_length=5)
 
@@ -66,13 +54,74 @@ class OperationProgramStatus(models.Model):
 
 class OperationProgram(models.Model):
     start_at = models.DateField("Fecha de inicio", unique=True)
-    op_type = models.ForeignKey(OperationProgramType, related_name="Tipo de programa de operación")
+    op_type = models.ForeignKey(OperationProgramType, related_name="ops", on_delete=models.PROTECT)
 
 
 class Organization(models.Model):
-    name = models.TextField("Nombre", max_length=30)
     created_at = models.DateTimeField()
-    op_type = models.ForeignKey(OperationProgramType, related_name="Tipo de programa de operación", blank=False)
-    default_counterpart = models.ForeignKey("self", related_name="Contraparte por defecto", blank=True)
-    default_user_contact = models.ForeignKey(User, related_name='Usuario', blank=True)
+    op_type = models.ForeignKey(OperationProgramType, related_name="organizations", blank=False,
+                                on_delete=models.PROTECT)
+    default_counterpart = models.ForeignKey("self", related_name="organizations", blank=True, on_delete=models.PROTECT)
+    default_user_contact = models.ForeignKey("User", blank=True, related_name="organizations", on_delete=models.PROTECT)
 
+
+class User(AbstractUser):
+    """User model."""
+
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    organization = models.ForeignKey(Organization, on_delete=models.PROTECT, null=True)
+    access_to_ops = models.BooleanField(default=False)
+    access_to_organizations = models.BooleanField(default=False)
+    access_to_users = models.BooleanField(default=False)
+
+
+class ChangeOPRequest(models.Model):
+    created_at = models.DateTimeField("Fecha de Creación", default=timezone.now)
+    creator = models.ForeignKey(User, related_name="change_op_requests", on_delete=models.PROTECT, blank=False)
+    op = models.ForeignKey(OperationProgram, related_name="change_op_requests", on_delete=models.PROTECT)
+    status = models.ForeignKey(OperationProgramStatus, related_name="change_op_requests", on_delete=models.PROTECT)
+    counterpart = models.ForeignKey(Organization, related_name="change_op_request", on_delete=models.PROTECT)
+    contract_type = models.ForeignKey(ContractType, related_name="change_op_request", on_delete=models.PROTECT)
+    title = models.CharField("Titulo", max_length=50)
+    message = models.TextField("Mensaje")
+    updated_at = models.DateTimeField("Fecha de actualización", default=timezone.now)
+
+
+class ChangeOPRequestFile(models.Model):
+    file = models.FileField("Archivo")
+    change_op_request = models.ForeignKey(ChangeOPRequest, related_name="change_op_request_files",
+                                          on_delete=models.PROTECT)
+
+
+class ChangeOPRequestMessage(models.Model):
+    created_at = models.DateTimeField("Fecha de creación", default=timezone.now)
+    creator = models.ForeignKey(User, related_name="change_op_request_messages", on_delete=models.PROTECT, blank=False)
+    message = models.TextField("Mensaje")
+
+
+class ChangeOPRequestMessageFile(models.Model):
+    file = models.FileField("Archivo")
+    change_op_request_message = models.ForeignKey(ChangeOPRequestMessage,
+                                                  related_name="change_op_request_message_files",
+                                                  on_delete=models.PROTECT)
+
+
+class StatusLog(models.Model):
+    created_at = models.DateTimeField("Fecha de creación", default=timezone.now)
+    previous_status = models.ForeignKey(OperationProgramStatus, related_name="status_logs", on_delete=models.PROTECT)
+    new_status = models.ForeignKey(OperationProgramStatus, related_name="+", on_delete=models.PROTECT)
+    change_op_request = models.ForeignKey(ChangeOPRequest, related_name="+", on_delete=models.PROTECT)
+
+    class OPChangeLog(models.Model):
+        created_at = models.DateField("Fecha de creación", default=timezone.now)
+        creator = models.ForeignKey(User, related_name="op_change_logs", on_delete=models.PROTECT, blank=False)
+        previous_op = models.ForeignKey(OperationProgram, related_name="+", on_delete=models.PROTECT)
+        new_op = models.ForeignKey(OperationProgram, related_name="+", on_delete=models.PROTECT)
+        change_op_request = models.ForeignKey(ChangeOPRequest, related_name="op_change_logs", on_delete=models.PROTECT)
