7825dfa7244f230192a8f9c06928cd018ed58159
Bastianleaf <bastian.martinez@ing.uchile.cl>
2021-10-25 18:22:51 -0300

    Refactoring

--- rest_api/permissions.py
+++ rest_api/permissions.py
@@ -7,8 +7,6 @@ def is_in_group(user, group_name):
     Takes a user and a group name, and returns `True` if the user is in that group.
     """
     try:
-        print(user)
-        print(group_name)
         return Group.objects.get(name=group_name).user_set.filter(id=user.id).exists()
     except Group.DoesNotExist:
         return None
