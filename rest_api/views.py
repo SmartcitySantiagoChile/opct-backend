15b9d1b632e3c4b37d2ea0a31cd8bc5f52675443
Bastianleaf <bastian.martinez@ing.uchile.cl>
2021-12-21 16:08:53 -0300

    Update Operation Program update method

--- rest_api/views.py
+++ rest_api/views.py
@@ -165,6 +165,22 @@ class OperationProgramViewSet(viewsets.ModelViewSet):
         except OperationProgram.DoesNotExist:
             raise NotFound()
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = OperationProgramCreateSerializer(
+            instance, context={"request": request}, data=request.data, partial=partial
+        )
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
 
 class OperationProgramTypeViewSet(viewsets.ReadOnlyModelViewSet):
     """
