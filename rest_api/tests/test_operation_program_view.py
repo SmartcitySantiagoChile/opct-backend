edffcda78b9bb7a069855a191c962dc9bc51e697
cephe <cephei.1313@gmail.com>
2022-06-18 13:03:28 -0400

    fix tests

--- rest_api/tests/test_operation_program_view.py
+++ rest_api/tests/test_operation_program_view.py
@@ -1,29 +1,35 @@
+import json
+
 from django.urls import reverse
 from django.utils import timezone
-from rest_framework.status import (
-    HTTP_200_OK,
-    HTTP_201_CREATED,
-    HTTP_204_NO_CONTENT,
-    HTTP_403_FORBIDDEN,
-    HTTP_404_NOT_FOUND,
-    HTTP_409_CONFLICT,
-)
+from rest_framework.request import Request
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_403_FORBIDDEN, \
+    HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
+from rest_framework.test import APIRequestFactory
 
-from rest_api.models import OperationProgram, OPChangeDataLog
-from .test_views_base import BaseTestCase
+from rest_api.models import OperationProgram, OPChangeLog, OperationProgramType
+from rest_api.serializers import OperationProgramSerializer
+from rest_api.tests.test_views_base import BaseTestCase
 
 
 class OperationProgramViewSetTest(BaseTestCase):
 
+    def setUp(self):
+        super(OperationProgramViewSetTest, self).setUp()
+        self.op_program = self.create_operation_program('2022-01-01', OperationProgramType.BASE)
+        self.create_operation_program('2022-03-01', OperationProgramType.MODIFIED)
+        self.create_operation_program('2022-07-01', OperationProgramType.BASE)
+        self.create_operation_program('2022-09-17', OperationProgramType.SPECIAL)
+
     # ------------------------------ helper methods ------------------------------ #
     def operation_program_list(self, client, data, status_code=HTTP_200_OK):
         url = reverse("operationprogram-list")
-        return self._make_request(client, self.GET_REQUEST, url, data, status_code)
+        return self._make_request(client, self.GET_REQUEST, url, data, status_code, json_process=True)
 
     def operation_program_retrieve(self, client, pk, status_code=HTTP_200_OK):
         url = reverse("operationprogram-detail", kwargs=dict(pk=pk))
         data = dict()
-        return self._make_request(client, self.GET_REQUEST, url, data, status_code)
+        return self._make_request(client, self.GET_REQUEST, url, data, status_code, json_process=True)
 
     def operation_program_create(self, client, data, status_code=HTTP_201_CREATED):
         url = reverse("operationprogram-list")
@@ -40,55 +46,53 @@ class OperationProgramViewSetTest(BaseTestCase):
 
     # ------------------------------ tests ---------------------------------------- #
     def test_list_with_group_permissions(self):
-        self.login_op_user()
-        self.operation_program_list(self.client, {})
+        self.login_dtpm_admin_user()
+        json_response = self.operation_program_list(self.client, {})
+        self.assertEqual(4, len(json_response['results']))
 
     def test_list_without_group_permissions(self):
-        self.login_user_user()
+        self.login_dtpm_viewer_user()
         self.operation_program_list(self.client, {}, HTTP_403_FORBIDDEN)
 
-        self.login_organization_user()
-        self.operation_program_list(self.client, {}, HTTP_403_FORBIDDEN)
-
+    def test_list_without_active_session(self):
         self.client.logout()
         self.operation_program_list(self.client, {}, HTTP_403_FORBIDDEN)
 
     def test_retrieve_with_group_permissions(self):
-        op = self.create_op("2021-10-29")
-        self.login_op_user()
-        self.operation_program_retrieve(self.client, op.pk)
-        op.delete()
+        self.login_dtpm_admin_user()
+        json_response = self.operation_program_retrieve(self.client, self.op_program.pk)
+
+        factory = APIRequestFactory()
+        request = factory.get('/')
+        serializer_context = {'request': Request(request)}
+
+        self.assertDictEqual(OperationProgramSerializer(self.op_program, context=serializer_context).data,
+                             json_response)
 
     def test_retrieve_with_group_permissions_and_logs(self):
-        op = self.create_op("2021-10-29")
-        self.login_op_user()
-        op_change_data_log = OPChangeDataLog(
-            user=self.op_user,
-            op=op,
-            created_at=timezone.now(),
-            previous_data={},
-            new_data={},
-        )
-        op_change_data_log.save()
-        self.operation_program_retrieve(self.client, op.pk)
-        op_change_data_log.delete()
-        op.delete()
+        self.login_dtpm_admin_user()
+        OPChangeLog.objects.create(user=self.dtpm_admin_user, operation_program=self.op_program,
+                                   created_at=timezone.now(), previous_data={}, new_data={})
+        json_response = self.operation_program_retrieve(self.client, self.op_program.pk)
+
+        factory = APIRequestFactory()
+        request = factory.get('/')
+        serializer_context = {'request': Request(request)}
+        expected_response = json.loads(
+            json.dumps(OperationProgramSerializer(self.op_program, context=serializer_context).data))
+        expected_response['logs'][0]['user'] = expected_response['logs'][0]['user']['url']
+        self.assertEqual(expected_response, json_response)
 
     def test_retrieve_without_group_permissions(self):
-        op = self.create_op("2021-10-29")
+        self.login_dtpm_viewer_user()
+        self.operation_program_retrieve(self.client, self.op_program.pk, HTTP_403_FORBIDDEN)
 
-        self.login_user_user()
-        self.operation_program_retrieve(self.client, op.pk, HTTP_403_FORBIDDEN)
-
-        self.login_organization_user()
-        self.operation_program_retrieve(self.client, op.pk, HTTP_403_FORBIDDEN)
-
+    def test_retrieve_without_active_session(self):
         self.client.logout()
-        self.operation_program_retrieve(self.client, op.pk, HTTP_403_FORBIDDEN)
-        op.delete()
+        self.operation_program_retrieve(self.client, self.op_program.pk, HTTP_403_FORBIDDEN)
 
     def test_create_with_group_permissions(self):
-        self.login_op_user()
+        self.login_dtpm_admin_user()
         start_at_date = "2021-01-01"
         op_type_url = reverse("operationprogramtype-detail", kwargs=dict(pk=1))
         data = {
@@ -96,92 +100,67 @@ class OperationProgramViewSetTest(BaseTestCase):
             "op_type": op_type_url,
         }
         self.operation_program_create(self.client, data)
-        OperationProgram.objects.get(start_at=start_at_date).delete()
+        self.assertTrue(OperationProgram.objects.filter(start_at=start_at_date).exists())
 
     def test_create_without_group_permissions(self):
-        start_at_date = "2021-01-01"
-        op_type_url = reverse("operationprogramtype-detail", kwargs=dict(pk=1))
-        data = {
-            "start_at": start_at_date,
-            "op_type": op_type_url,
-        }
+        self.login_dtpm_viewer_user()
+        self.operation_program_create(self.client, {}, HTTP_403_FORBIDDEN)
 
-        self.login_user_user()
-        self.operation_program_create(self.client, data, HTTP_403_FORBIDDEN)
-
-        self.login_organization_user()
-        self.operation_program_create(self.client, data, HTTP_403_FORBIDDEN)
-
+    def test_create_without_active_session(self):
         self.client.logout()
-        self.operation_program_create(self.client, data, HTTP_403_FORBIDDEN)
+        self.operation_program_create(self.client, {}, HTTP_403_FORBIDDEN)
 
     def test_update_with_group_permissions(self):
-        self.login_op_user()
-        start_at_date = "2021-01-01"
-        op = self.create_op(start_at_date)
+        self.login_dtpm_admin_user()
 
-        new_start_at_date = "2021-01-02"
-        op_type_url = reverse("operationprogramtype-detail", kwargs=dict(pk=1))
+        new_start_at_date = "2022-01-03"
+        op_type_url = reverse("operationprogramtype-detail", kwargs=dict(pk=2))
         data = {
             "start_at": new_start_at_date,
             "op_type": op_type_url,
         }
-        self.operation_program_patch(self.client, op.pk, data)
-        op.delete()
+        self.operation_program_patch(self.client, self.op_program.pk, data)
+
+        # check log
+        self.assertEqual(1, OPChangeLog.objects.filter(operation_program=self.op_program).count())
+        op_change_data_log = OPChangeLog.objects.first()
+        self.assertJSONEqual('{"date": "2022-01-01", "op_type": "Base"}', op_change_data_log.previous_data)
+        self.assertJSONEqual('{"date": "2022-01-03", "op_type": "Modificado"}', op_change_data_log.new_data)
+        self.assertEqual(self.dtpm_admin_user, op_change_data_log.user)
 
     def test_update_without_group_permissions(self):
-        start_at_date = "2021-01-01"
-        op = self.create_op(start_at_date)
+        self.login_dtpm_viewer_user()
+        self.operation_program_patch(self.client, self.op_program.pk, {}, HTTP_403_FORBIDDEN)
 
-        new_start_at_date = "2021-01-02"
-        op_type_url = reverse("operationprogramtype-detail", kwargs=dict(pk=1))
-        data = {
-            "start_at": new_start_at_date,
-            "op_type": op_type_url,
-        }
-
-        self.login_user_user()
-        self.operation_program_patch(self.client, op.pk, data, HTTP_403_FORBIDDEN)
-
-        self.login_organization_user()
-        self.operation_program_patch(self.client, op.pk, data, HTTP_403_FORBIDDEN)
-
+    def test_update_without_active_session(self):
         self.client.logout()
-        self.operation_program_patch(self.client, op.pk, data, HTTP_403_FORBIDDEN)
-        op.delete()
+        self.operation_program_patch(self.client, self.op_program.pk, {}, HTTP_403_FORBIDDEN)
 
-    def test_delete_with_permissions_and_no_change_op_request(self):
-        self.login_op_user()
-        date = "2021-05-01"
-        op = self.create_op(date)
-        self.operation_program_delete(self.client, op.pk)
-        op.delete()
+    def test_delete_with_permissions_and_no_op_processes_related(self):
+        self.login_dtpm_admin_user()
+        self.operation_program_delete(self.client, self.op_program.pk)
 
     def test_delete_with_permissions_and_not_found(self):
-        self.login_op_user()
-        self.operation_program_delete(self.client, 1, HTTP_404_NOT_FOUND)
+        self.login_dtpm_admin_user()
+        self.operation_program_delete(self.client, 1000, HTTP_404_NOT_FOUND)
 
-    def test_delete_with_permissions_with_change_op_request(self):
-        self.login_op_user()
-        date = "2021-05-01"
-        op = self.create_op(date)
-        change_op_request = self.create_change_op_request(
-            self.op_user, op, self.organization_base, self.contract_type
-        )
-        self.operation_program_delete(self.client, op.pk, HTTP_409_CONFLICT)
-        change_op_request.delete()
-        op.delete()
+    def test_delete_with_permissions_and_op_processes_related(self):
+        self.login_dtpm_admin_user()
+        self.create_op_process(self.dtpm_viewer_user, self.op1_organization, self.op1_contract_type,
+                               op=self.op_program)
+        self.operation_program_delete(self.client, self.op_program.pk, HTTP_409_CONFLICT)
+
+    def test_delete_with_permissions_and_op_request_related(self):
+        self.login_dtpm_admin_user()
+        op_proces_obj = self.create_op_process(self.dtpm_viewer_user, self.op1_organization, self.op1_contract_type,
+                                               op=None)
+        self.create_op_request(self.dtpm_admin_user, op_proces_obj, op=self.op_program)
+        self.operation_program_delete(self.client, self.op_program.pk, HTTP_409_CONFLICT)
 
     def test_delete_without_permissions(self):
-        date = "2021-05-01"
-        op = self.create_op(date)
+        self.login_dtpm_viewer_user()
+        self.operation_program_delete(self.client, self.op_program.pk, HTTP_403_FORBIDDEN)
 
-        self.login_user_user()
-        self.operation_program_delete(self.client, op.pk, HTTP_403_FORBIDDEN)
-
-        self.login_organization_user()
-        self.operation_program_delete(self.client, op.pk, HTTP_403_FORBIDDEN)
-
+    def test_delete_without_active_session(self):
         self.client.logout()
-        self.operation_program_delete(self.client, op.pk, HTTP_403_FORBIDDEN)
-        op.delete()
+        self.operation_program_delete(self.client, self.op_program.pk, HTTP_403_FORBIDDEN)
