b9a476f77a231b72e2f455c3861db3e5b278174a
cephe <cephei.1313@gmail.com>
2022-06-20 03:45:30 -0400

    refactoring

--- opct/settings.py
+++ opct/settings.py
@@ -171,7 +171,9 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
-    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend"
+    ],
 }
 
 # django-rq task queueing
